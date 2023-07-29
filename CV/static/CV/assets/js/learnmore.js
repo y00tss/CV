d278ef2afa5fb40ab412a7549b12ee875e83d58c
y00tss <shepel9119@gmail.com>
2023-07-29 14:29:41 +0300

    Update 0.1v

---
+++ CV/static/CV/assets/js/learnmore.js
@@ -0,0 +1,14 @@
+
+function showFullInfo() {
+    var fullInfoElement = document.getElementById("full-info");
+    var button = document.querySelector("#one .button");
+
+    if (fullInfoElement.style.display === "none") {
+        fullInfoElement.style.display = "block";
+        button.textContent = "Show Less";
+    } else {
+        fullInfoElement.style.display = "none";
+        button.textContent = "Learn More";
+    }
+}
+
